bd34753af71d323e5d6f024d6b3e7734d12fec71
Ankur Garg <agarg12@ncsu.edu>
2017-08-31 15:36:27 -0400

    Updating & restrucuturing

--- HW1/src/fss_hw1.py
+++ HW1/src/fss_hw1.py
@@ -1,21 +1,21 @@
 import sys
 import os
 import argparse
-# Take filename as input or hardcode!
-# Declare List variables
-true_length = [0]
-filtered_length = [0]
-data_types = []
-ignore_list = []
-main_list = []
+
+
 
 class CSVReader(object):
 	def __init__(self, filename):
-		self.path = os.path.basename(filename)
+		self.path = os.path.abspath(filename)
 		assert (os.path.exists(self.path)), "file doesn't exist"
+		self.main_list = []
+		self.true_length = [0]
+		self.filtered_length = [0]
+		self.data_types = []
+		self.ignore_list = []
 
-# test: l = 'outlook,?temp,$meta,play'
-	def handle_header(line,data_types,ignore_list,main_list,true_length,filtered_length):
+	# test: l = 'outlook,?temp,$meta,play'
+	def handle_header(self, line):
 		#comment filtered out
 		header_line = line.split('#')[0]
 		header_line.strip()
@@ -23,52 +23,52 @@ class CSVReader(object):
 		#	header_line = header_line[0:-1]
 		# store the lengths for checking the rows
 		columns = header_line.split(',')
-		true_length[0] = len(columns)
+		self.true_length[0] = len(columns)
 		# set the data types for each column
 		for i in range(len(columns)):
 			columns[i] = columns[i].strip()
 			if columns[i][0] == '?':
-				ignore_list.append(i)		# ? ==> ignore
+				self.ignore_list.append(i)		# ? ==> ignore
 			elif columns[i][0] == '$':
-				data_types.append('int')
+				self.data_types.append('int')
 			else:
-				data_types.append('str')
+				self.data_types.append('str')
 		# filter out the columns to ignore from the header
-		for i in ignore_list:
+		for i in self.ignore_list:
 			columns.pop(i)
-		filtered_length[0] = len(columns)
+		self.filtered_length[0] = len(columns)
 		return columns
 
-	def handle_row(line,data_types,ignore_list,main_list):
+	def handle_row(self, line):
 		# remove comments
 		row_line = line.split('#')[0]
 		row_line.strip()
 		# check for lengths
 		columns = row_line.split(',')
-		if len(columns) != true_length[0]:
+		if len(columns) != self.true_length[0]:
 			return []
 		# remove colunms to ignore
-		for i in ignore_list:
+		for i in self.ignore_list:
 			columns.pop(i)
-		if len(columns) != filtered_length[0]:
+		if len(columns) != self.filtered_length[0]:
 			return []
 		# if column is of correct length check for data types
 		for i in range(len(columns)):
 			columns[i] = columns[i].strip()
-			if data_types[i] == 'int':
+			if self.data_types[i] == 'int':
 				try:
 					columns[i] = int(columns[i])
 				except:
 					return []
-			if data_types[i] == 'str':
+			if self.data_types[i] == 'str':
 				try:
 					columns[i] = str(columns[i])
 				except:
 					return []
-		print columns
+		#print columns
 		return columns
 
-	def handle_file(self):
+	def parse(self):
 		f = open(self.path)
 		lines = f.readlines()
 		row_start = 1
@@ -77,9 +77,9 @@ class CSVReader(object):
 		if l[-1] == ',':			# merge two lines if line ends with ','
 			l = l+lines[1]
 			row_start = 2
-		header = handle_header(l,data_types,ignore_list,main_list,true_length,filtered_length)
+		header = self.handle_header(l)
 		if len(header) != 0:
-			main_list.append(header)
+			self.main_list.append(header)
 		# handle rows of the file, check for merge two lines
 		i=row_start
 		while i < len(lines):
@@ -87,9 +87,9 @@ class CSVReader(object):
 			if l[-1] == ',':		# merge two lines if line ends with ','
 				l = l + lines[i+1]
 				i = i + 1
-			row = handle_row(l,data_types,ignore_list,main_list)
+			row = self.handle_row(l)
 			if len(row) != 0:
-				main_list.append(row)
+				self.main_list.append(row)
 			i = i + 1
 		f.close()
 		return
@@ -100,5 +100,7 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	reader = CSVReader(args.filename)
-	#reader.parse()
+	reader.parse()
+	for i in reader.main_list:
+		print i
 
