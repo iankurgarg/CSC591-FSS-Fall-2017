f78e290380b79d8d7ab7de223d0e9d8eee28ec99
Sanket Shahane <svshahan@ncsu.edu>
2017-09-28 16:07:58 -0400

    Cleaning and resolving code confusions
    About 4-5 line of code is still unclear on what to replace instead of them. Comments are written where problem exists
    Pending tasks: 1. Clear confusions. 2. Test

--- HW4/Sdtree.py
+++ HW4/Sdtree.py
@@ -7,7 +7,7 @@ import Config as the
 import Num as num
 import Table as t
 
-class sdtree:
+class Sdtree:
 	def __init__(self):
 		self._t = None
 		self._kids = []
@@ -27,7 +27,7 @@ class sdtree:
 		self.stats = num.updates(t.rows, yfun)
 		return
 
-	def order(t,y):
+	def order(self,t,y):
 		
 		def xpect(col):
 			tmp = 0
@@ -46,20 +46,25 @@ class sdtree:
 		for _,h in enumerate(t.x.cols):
 			out.append(whatif(h,y))
 		# how to write function (x,y) return x.key < y.key
+		# sorted(out) ?
 		table.sort(out,x.key<y.key)
 		# the last line too
 		return lst.collect(out, x.val)
 
-	def grow1(above,yfun,rows,lvl,b4,pos,attr,val):
-
+	def grow1(self,above,yfun,rows,lvl,b4,pos,attr,val):
 		def pad():
+			# is the below statement str('| '+lvl).format("%-20s")
 			return str.fmt("%-20s",string.rep('| ',lvl))
 
 		def likeAbove():
+			# what is our version of tbl.copy? Table.py does not have a copy function. Shoud we just return
+			# rows = copy.copy(above._t)
+			# return rows
 			return tbl.copy(above._t, rows)
 
 		if len(rows) >= the.tree.min:
 			if lvl <= the.tree.maxDepth:
+				# This line looks unfamiliar
 				here = (lvl==0 and above or create(likeAbove(), yfun, pos, attr, val)
 				if here.stats.sd < b4:
 					if lvl > 0:
@@ -78,33 +83,38 @@ class sdtree:
 						if len(rows) < len(row):
 							grow1(here,yfun,rows1,lvl+1,here.stats.sd,cut.pos,cut.what,val)
 
+	def grow(self,t,y):
+		yfun = tbl[y](t)
+		root = create(t,yfun)
+		grow1(root,yfun,t.rows,0,10^32)
+		return root
+
+	def tprint(self,tr, lvl=0):
+		def pad():
+			return '| '+str(lvl-1)
+		def left(x):
+			return str('%-20s',x)
+		suffix=""
+		if len(tr._kinds) ==- or lvl==0:
+			suffix = str("n=%s mu=%-.2f sd=%-.2f", tr.stats.n, tr.stats.mu, tr.stats.sd)
+		if lvl = 0:
+			print "\n" + suffix
+		else:
+			print left(pad())+tr.attr+"="+tr.val+"\t:"+suffix
+		for j in range(1,len(tr._kinds)):
+			tprint(tr._kinds[j],lvl+1)
 		return
 
-		def grow(t,y):
-			yfun = tbl[y](t)
-			root = create(t,yfun)
-			grow1(root,yfun,t.rows,0,10^32)
-			return root
+	def leaf(self,tr,cells,lvl=0):
+		for j,kid in enumerate(tr._kids):
+			pos,val = kid.pos, kid.val
+			if cells[kid.pos] == kid.val:
+				# don't know where this bins is coming from
+				return leaf(kid, cells, bins,lvl+1)
+		return tr
 
-		def tprint(tr, lvl=0):
-			def pad():
-				return '| '+str(lvl-1)
-			def left(x):
-				return str('%-20s',x)
-			suffix=""
-			if len(tr._kinds) ==- or lvl==0:
-				suffix = str("n=%s mu=%-.2f sd=%-.2f", tr.stats.n, tr.stats.mu, tr.stats.sd)
-			if lvl = 0:
-				print "\n" + suffix
-			else:
-				print left(pad())+tr.attr+"="+tr.val+"\t:"+suffix
-			for j in range(1,len(tr._kinds)):
-				tprint(tr._kinds[j],lvl+1)
-
-		def leaf(tr,cells,lvl=0):
-			for j,kid in enumerate(tr._kids):
-				pos,val = kid.pos, kid.val
-				if cells[kid.pos] == kid.val:
-					# don't know where this bins is coming from
-					return leaf(kid, cells, bins,lvl+1)
-			return tr
+if __name__=="__main__":
+	# Main function call to run the script
+	tree_build = Sdtree()
+	tree_build.create()
+	
