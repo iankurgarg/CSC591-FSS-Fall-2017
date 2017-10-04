d1421fb3b40c4040367551e965a2d8664dd81139
Ankur Garg <agarg12@ncsu.edu>
2017-10-04 00:14:15 -0400

    big fixes  to contrasts

--- HW5/src/Contrast.py
+++ HW5/src/Contrast.py
@@ -14,61 +14,67 @@ import copy
 # 	def __init__(self):
 # 		return
 
+class Branch(object):
+	has = None
+	lst = []
+
 def has(branch):
 	out = []
-	for i in range(1,len(branch)):
-		step = branch[i]
-		out.append({'attr':step.attr, 'val':step.val})
-	return
+	for i in range(1,len(branch.lst)):
+		step = branch.lst[i]
+		out.append({'attr':step['attr'], 'val':step['val']})
+	return out
 
 def have(branches):
-	for _,branch in enumerate(branches): #should be a list
+	for _, branch in enumerate(branches): #should be a list
 		branch.has = has(branch)
 	return branches
 
 def branches1(tr, out, b):
 	if tr.attr:
-		b.append({'attr'=tr.attr,'val'=tr.val,'_stats'=tr.stats})
-	if len(b) > 0:
+		b.lst.append({'attr': tr.attr,'val' : tr.val, '_stats': tr.stats})
+	if len(b.lst) > 0:
 		out.append(b)
 	for _,kid in enumerate(tr._kids):
-		branches1(kid,out,copy.deepcopy(b))
-	return b # list of dictonaries
+		branches1(kid, out, copy.deepcopy(b))
+	return out # list of dictonaries
 
 def branches(tr):
-	return have(branches1(tr,{},{}))
+	return have(branches1(tr,[],Branch()))
 
 def member2(twins0, twins):
-	for _,twin1 in enumerate(twins):
+	for _, twin1 in enumerate(twins):
 		if twin0.attr == twin1.attr and twin0.val == twin1.val:
 			return True
 	return False
 
 def delta(t1, t2):
+	print "here"
 	out = []
-	for _,twin in enumerate(t1):
-		if not member2(twin,t2):
-			out.append((twin.attr,twin.val))
+	for _, twin in enumerate(t1):
+		if not member2(twin, t2):
+			out.append((twin.attr, twin.val))
 	return out # list of tuples
 
 def contrasts(branches, better):
-	for i,branch1 in enumerate(branches):
+	for i, branch1 in enumerate(branches):
 		out = []
-		for j,branch2 in enumerate(branches):
-			if i!=j:
-				num1 = branch1[-1]._stats
-				num2 = branch2[-1]._stats
+		for j, branch2 in enumerate(branches):
+			if i != j:
+				num1 = branch1.lst[-1]['_stats']
+				num2 = branch2.lst[-1]['_stats']
 				if better(num2.mu, num1.mu):
-					if not Num.same(num1,num2): # write a static function in Num.py called same, hedges, ttest1, ttest
+					print "1"
+					if not num1.same(num2):#Num.same(num1,num2): # write a static function in Num.py called same, hedges, ttest1, ttest
 						inc = delta(branch2.has,branch1.has)
 						if len(inc) > 0:
 							out.append( {'i':i,'j':j,'ninc':len(inc),'muinc':num2.mu - num1.mu,'inc':inc, 'branch1':branch1.has,'mu1':num1.mu,'branch2':branch2.has,'mu2':num2.mu} )
 		print ""
 		# below sorted line needs correction @Ankur
-		sorted(out, key={lambda (x,y) : x.muinc < y.muinc})
-		print i,'max mu',out[0]
-		sorted(out, key={lambda (x,y) : x.ninc < y.ninc})
-		print i,'min inc',out[1]
+		sorted(out, key=lambda x, y : x.muinc < y.muinc)
+		print i, 'max mu', out[0]
+		sorted(out, key=lambda x, y : x.ninc < y.ninc)
+		print i, 'min inc', out[0]
 	return
 
 def more(x,y):
@@ -78,10 +84,23 @@ def less(x,y):
 	return x < y
 
 def plans(branches):
-	return contrasts(branches,more)
+	return contrasts(branches, more)
 
 def monitors(branches):
-	return contrasts(branches,less)
+	return contrasts(branches, less)
+
+if __name__ == '__main__':
+	filename = sys.argv[1]
+	tbl = Table()
+	tbl.fromCSV(filename)
+	discretized_table = tbl.discretizeRows()
+	
+	tree_build = Sdtree(discretized_table, yfun=discretized_table.dom())
+	tree_build.grow()
+	b = branches(tree_build)
+	# print b
+
+	plans = contrasts(b, lambda x, y: x > y)
 
 # need to figure out the main function like before
 # and what is the class of branches.
